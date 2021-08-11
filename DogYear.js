eb5926d378aea5b75aaaf3f6496ca1492bd051d9
Rexford Koomson <86994591+rexfordkode@users.noreply.github.com>
2021-08-11 22:25:20 +0000

    Add files via upload

---
+++ DogYear.js
@@ -0,0 +1,30 @@
+//My Age
+let myAge = 25;
+
+//Asigning 2 to earlyYears for dogs
+let earlyYears = 2;
+
+//To calculate 2 years of dogs years
+earlyYears = earlyYears * 10.5;
+
+
+myAge = myAge - 2;
+//Variable to save value of later year 
+let laterYears = myAge;
+
+laterYears = laterYears * 4;
+
+//Adding early years and later years to make dog years
+let myAgeInDogYears = earlyYears + laterYears;
+
+//Creating a name variable and converting into lower case
+let myName = 'Rexford'.toLowerCase();
+
+//This console is to display age of human in years against that of dogs
+console.log(`My name is ${myName}. I am  ${myAge} years old in human years which is ${laterYears} years old in dog years.`);
+
+
+
+
+
+
